a3ddbf9167a7408d7743d1ad41b677e430ac75c5
danilwladich <frosbutte1@gmail.com>
2024-01-01 18:55:58 +0100

    get auth user return type

--- lib/get-auth-user.ts
+++ lib/get-auth-user.ts
@@ -1,5 +1,6 @@
+import { UserWithoutPassword } from "@/types/UserWithoutPassword";
 import { NextRequest } from "next/server";
 
-export function getAuthUser(req: NextRequest) {
+export function getAuthUser(req: NextRequest): UserWithoutPassword {
 	return JSON.parse(req.headers.get("x-auth-user") || "");
 }
