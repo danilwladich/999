bd57f85cdb97c89f5c4fb76c8f7201696e1e4160
Daniel <118133808+danilwladich@users.noreply.github.com>
2024-02-10 18:22:58 +0100

    auth me error

--- components/providers/auth-provider.tsx
+++ components/providers/auth-provider.tsx
@@ -5,6 +5,7 @@ import { useAuthStore } from "@/hooks/use-auth-store";
 import type { UserWithoutPassword } from "@/types/UserWithoutPassword";
 
 import { AppLoader } from "@/components/ui/app-loader";
+import { useEffect } from "react";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
 	const { data, isLoading, error } = useClientFetching<UserWithoutPassword>(
@@ -14,12 +15,17 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
 	const { isChecked, setUser } = useAuthStore();
 
+	useEffect(() => {
+		if (!error && data) {
+			setUser(data);
+		} else {
+			setUser(null);
+		}
+	}, [data, error]);
+
+	// First fetching loader
 	if (isLoading && !isChecked) {
 		return <AppLoader />;
-	}
-
-	if (!error && data) {
-		setUser(data);
 	}
 
 	return children;
